9e35f359f9ed4ab5ccf8fc698585932963f57b76
V_A_N <V_A_N-1_94@mail.ru>
2024-03-31 11:28:01 +0300

    HW 20.2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from catalog.views import home, contacts
+from catalog import views
+from catalog.views import home, contacts, product
 
 urlpatterns = [
     path('', home),
     path('contact/', contacts),
+    path('product/', product),
+    path('product/<int:pk>', views.product, name='product'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
+from catalog.models import Product, Category
+
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    category_list = Category.objects.all()
+    context = {
+        'object_list': category_list
+    }
+    return render(request, 'catalog/home.html', context)
 
 
 def contacts(request):
@@ -12,3 +18,11 @@ def contacts(request):
         massage = request.POST.get('message')
         print(f"Имя: {name}, Телефон: {phone}, Сообщение: {massage}")
     return render(request, 'catalog/contacts.html')
+
+
+def product(request, pk):
+    product = Product.objects.get(pk=pk)
+    # context = {
+    #     'category': Product.objects.all()
+    # }
+    return render(request, 'catalog/product.html')
