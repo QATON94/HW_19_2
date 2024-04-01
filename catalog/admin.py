349483dfeb45646100ca2ae678e22b66e57ecf0b
V_A_N <V_A_N-1_94@mail.ru>
2024-04-01 22:17:13 +0300

    fix code

--- catalog/admin.py
+++ catalog/admin.py
@@ -11,7 +11,6 @@ class ProductAdmin(admin.ModelAdmin):
     search_fields = ('name', 'description',)
 
 
-
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
--- catalog/templatetags/image_teg.py
+++ catalog/templatetags/image_teg.py
@@ -2,6 +2,7 @@ from django import template
 
 register = template.Library()
 
+
 @register.simple_tag()
 def image_tag(data):
     if data:
